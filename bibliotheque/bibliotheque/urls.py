bfa5540b404d43519f05c543d2100e7c0dd2aa05
Devdingo <Othiombane@pyramide.com>
2026-04-18 13:50:52 +0000

    creation aplication

--- bibliotheque/bibliotheque/urls.py
+++ bibliotheque/bibliotheque/urls.py
@@ -1,7 +1,11 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tacos/',include('main.urls')),
 ]
+
+
+#URLS PROJET
---
+++ bibliotheque/main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('',views.home),
+    path('about',views.about),
+    
+]
+
+
+
+#URLS APPLICATION MAIN
